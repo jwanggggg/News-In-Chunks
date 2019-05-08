8dfe3d2e1a09148e0017dfcdd5fd1f97430af27c
Jonathan <jw4107@nyu.edu>
2019-05-08 16:01:38 -0400

    Restoring previous changes for scraping

--- News In Chunks/Controllers/ArticlesTableViewController.swift
+++ News In Chunks/Controllers/ArticlesTableViewController.swift
@@ -9,10 +9,9 @@
 import UIKit
 
 class ArticlesTableViewController: UITableViewController {
-    var articles: [Article] = Article.fetchArticles()
+    var articles: [Article] = []
 
     // MARK: - UITableViewDataSource
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return articles.count
     }
@@ -20,7 +19,7 @@ class ArticlesTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell", for: indexPath) as! ArticleTableViewCell
         let article = articles[indexPath.row]
-        
+        cell.thumbnailImageView.downloadImage(from: self.articles[indexPath.item].thumbnailFileName)
         cell.article = article
         return cell
     }
@@ -46,6 +45,79 @@ class ArticlesTableViewController: UITableViewController {
     override func viewDidLoad() {
         // set gradient background
         tableView.backgroundView = UIImageView(image: UIImage(named: "ViewControllerBackground.png"))
+        fetchArticles()
+    }
+    
+    func fetchArticles(){
+        let urlRequest = URLRequest(url: URL(string: "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=9d9eeff96c6f4f4989f1e892f700857a")!)
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data,response,error) in
+            
+            if error != nil {
+                print(error)
+                return
+            }
+            
+            self.articles = [Article]()
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String : AnyObject]
+                
+//                let articleName: String
+//                let articleDescription: String
+//                let thumbnailFileName: String
+//                let articleText: String
+//                let bookmarked: Bool
+                
+                if let articlesFromJson = json["articles"] as? [[String : AnyObject]] {
+                    for articleFromJson in articlesFromJson {
+                        var article = Article(articleName: "", articleDescription: "", thumbnailFileName: "", articleText: "", bookmarked: false)
+                        if let articleName = articleFromJson["title"] as? String, let author = articleFromJson["author"] as? String, let articleDescription = articleFromJson["description"] as? String, let thumbnailFileName = articleFromJson["urlToImage"] as? String,
+                            let articleText = articleFromJson["content"] as? String{
+                            
+                            article.articleName = articleName
+                            article.articleDescription = articleDescription
+                            article.thumbnailFileName = thumbnailFileName
+                            article.articleText = articleText
+                        }
+                        self.articles.append(article)
+                        print(article)
+                    }
+                }
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
+            } catch let error {
+                print(error)
+            }
+            
+            
+        }
+        
+        task.resume()
+        
+    }
+    
+}
+
+extension UIImageView {
+    
+    func downloadImage(from url: String) {
+        let urlRequest = URLRequest(url: URL(string: url)!)
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            
+            if (error != nil) {
+                print(error)
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.image = UIImage(data: data!)
+            }
+            
+        }
+        task.resume()
     }
     
 }
--- News In Chunks/Models/Article.swift
+++ News In Chunks/Models/Article.swift
@@ -9,11 +9,11 @@
 import Foundation
 
 struct Article {
-    let articleName: String
-    let articleDescription: String
-    let thumbnailFileName: String
-    let articleText: String
-    let bookmarked: Bool
+    var articleName: String
+    var articleDescription: String
+    var thumbnailFileName: String
+    var articleText: String
+    var bookmarked: Bool
 
     static func fetchArticles() -> [Article] {
         let a1 = Article(articleName: "Apple's WWDC keynote is set for June 3rd", articleDescription: "The annual developer conference again takes place at San Jose's McEnery Convention Center.", thumbnailFileName: "a1", articleText: "Apple has set the dates for this year's Worldwide Developers Conference. It takes place June 3rd to 7th at San Jose's McEnery Convention Center, the same venue as the last couple of years. At WWDC, Apple typically gives developers (and everyone else) an early look at upcoming versions of iOS, macOS, watchOS and tvOS. We might also learn a little more about Apple's services (it's likely to announce streaming video and news subscription plans this month) or even get a glimpse at new hardware. \n\n If you are interested in attending, you have until March 20th to register for the chance to buy tickets. Otherwise, you'll be able to watch live streams of the keynote (which takes place June 3rd) and conference through the WWDC app or the Apple Developer website.", bookmarked: true)
